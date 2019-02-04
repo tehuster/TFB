c2de733390de43bcdedad5b0efdcee1098cc4b38
Thomas Huster <tehuster@gmail.com>
2019-02-04 21:37:04 +0100

    Added StreamWriter

---
+++ Unity/PoZYX/Assets/Logging/LogManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Normal using statements here
+using System.IO;
+ 
+public class LogManager : MonoBehaviour
+{
+
+StreamWriter SW;
+
+private void Start() {
+	string path = "test.txt";
+	File.Create(path).Dispose();
+	//Write some text to the test.txt file
+	SW = new StreamWriter(path, true);
+	SW.AutoFlush = true;
+	SW.WriteLine("Test");
+	//SW.Close();
+}
+       
+ 
+void Update()
+{
+//Do stuff
+SW.WriteLine("Your Data");
+Debug.Log("logged");
+}
+ 
+void OnApplicationExit()
+{
+//Remember to close your StreamWriter!
+SW.Close();
+}
+}

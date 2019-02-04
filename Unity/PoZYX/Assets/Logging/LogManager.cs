0f3e0a21fd64042925057a9fcf2700347cc5bdd9
Thomas Huster <tehuster@gmail.com>
2019-02-04 22:49:33 +0100

    Added PoZYX + Motor logging!

--- Unity/PoZYX/Assets/Logging/LogManager.cs
+++ Unity/PoZYX/Assets/Logging/LogManager.cs
@@ -2,34 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 //Normal using statements here
+using System;
 using System.IO;
- 
+
 public class LogManager : MonoBehaviour
 {
 
-StreamWriter SW;
+    StreamWriter SW;
+	ParseDataToString ParseDataToString;
 
-private void Start() {
-	string path = "test.txt";
-	File.Create(path).Dispose();
-	//Write some text to the test.txt file
-	SW = new StreamWriter(path, true);
-	SW.AutoFlush = true;
-	SW.WriteLine("Test");
-	//SW.Close();
-}
+	private void Awake() {
+		ParseDataToString = GetComponent<ParseDataToString>();
+	}
+    private void Start()
+    {
+        string path = "USER_LOGS/test.txt";
+        CreateFile(path);
+		InitStreamWriter(path); //Don't forget to close;      
+        WriteLineToFile("Started");
+		InvokeRepeating("LogData", 0f, 0.04f);
+    }
+ 
+    
+
+    void LogData()
+    {	
+		WriteLineToFile(ParseDataToString.GetCurrentDataString());
+    }
+
+	void CreateFile(string fileName){
+		//Add timestamp
+		//Unique ID
+		//Identifier
+		//Location
+		File.Create(fileName).Dispose();
+	}
+
+	void InitStreamWriter(string fileName){
+		//SW.AutoFlush = true;
+		SW = new StreamWriter(fileName, true);		
        
- 
-void Update()
-{
-//Do stuff
-SW.WriteLine("Your Data");
-Debug.Log("logged");
-}
- 
-void OnApplicationExit()
-{
-//Remember to close your StreamWriter!
-SW.Close();
-}
+	}
+
+	void WriteLineToFile(string Line){
+		SW.WriteLine(Line);
+		SW.Flush();
+	}
+
+    void OnApplicationExit()
+    {
+        //Remember to close your StreamWriter!
+        SW.Close();
+    }
 }
---
+++ Unity/PoZYX/Assets/ParseDataToString.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ParseDataToString : MonoBehaviour {
+
+	public POZYXVariable POZYX; //Do i want POZYX or VIRTUAL TRANSFORM POS?
+	public MotorSpeed MSpeed;
+
+	public string GetCurrentDataString(){
+		string dataString;
+		DateTime currentDate = DateTime.Now;
+		dataString = currentDate.ToString("MM'/'dd'/'yyyy HH':'mm':'ss.fff") + ",";
+		dataString += POZYX.x + ",";
+		dataString += POZYX.y + ",";
+		dataString += POZYX.z + ",";
+		dataString += POZYX.yaw + ",";
+		foreach(int Motor in MSpeed.MotorsSpeed)
+        {
+            dataString += Motor + ",";
+        }	       
+		dataString += ";";
+		Debug.Log(dataString);
+		return dataString;
+	}
+}
